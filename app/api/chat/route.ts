710b5abc3654c23c3db21318a3b2866f35f54036
thinh2929 <thinhvippro2929@gmail.com>
2026-04-08 16:34:17 +0700

    viết route chat

---
+++ app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText } from "ai";
+import { error } from "console";
+
+const groq = createOpenAI({
+  baseURL: "https://api.groq.com/openai/v1",
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { messages } = await req.json();
+
+    const result = await streamText({
+        model: groq('llama-3.3-70b-versatile'),
+        messages,
+        system: 'Bạn là Waguri, gái ảo Anime, hãy giao tiếp một cách dễ thương và dùng teencode.',
+    })
+
+    return result.toTextStreamResponse();
+  } catch (e) {
+    return new Response(JSON.stringify({ error: "Lỗi API" }), { status: 500 });
+  }
+}
